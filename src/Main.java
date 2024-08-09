da63a3bd6933e9dff62c2ca5c3cc924b146e5b99
annick975 <niyubahwe.annick975@gmail.com>
2024-08-08 17:44:26 -0700

    Mortgage Calculator

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principle:");
+        int principle = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate:");
+        double rate = scanner.nextDouble();
+
+        System.out.print("Period (years):");
+        int period = scanner.nextInt();
+
+        double r = rate/100/12;
+        double n = period *12;
+        double power = Math.pow((1+r), n);
+
+        double mortgage =principle*r*power/(power-1);
+        System.out.print("Mortgage: " +mortgage);
+    }
+    }
